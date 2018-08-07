ad0dd56c62882d3396f400c36714a2472dbb56fc
Martino Wagey <martinowagey@gmail.com>
2018-08-07 16:31:57 +0700

    perbedaan var let const, dan add file function

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var message = "Hello World";
-
 let msg1 = "hello";
 
 const msg2 = "World";
 
-console.log(msg2);
+if (true) {
+  var message = 1;
+  let msg1 = 2;
+  console.log("latihan",msg1);
+}
+
+console.log(message);
+console.log(msg1);
