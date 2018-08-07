d856f927a3b7d04b48b242328c0521608f796886
Martino Wagey <martinowagey@gmail.com>
2018-08-07 16:54:20 +0700

    add if-else

---
+++ if-else.js
@@ -0,0 +1,14 @@
+function jalanSatu(){
+  var nilai = 3 ;
+  if (nilai === 3) {
+    console.log("Benar");
+  }else if(nilai === 2){
+    console.log("Dua");
+  }else{
+    console.log("Salah")
+  }
+  // if(nilai === 3)console.log("OK")
+  // else console.log("Bukan")
+}
+
+jalanSatu();
