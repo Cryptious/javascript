750ec2b9a436d9e42a740528f511e45ce09edf3c
Martino Wagey <martinowagey@gmail.com>
2018-08-14 13:54:47 +0700

    add array in objects

--- objects.js
+++ objects.js
@@ -3,9 +3,11 @@ function panggilObjects() {
     type: 'sedan',
     harga: 200000000,
     warna: "putih",
+    tahun: [2001,2002,2003,2004]
   }
   console.log(mobil);
   console.log(mobil.harga);
+  console.log(mobil.tahun[3]);
 }
 
 panggilObjects()
