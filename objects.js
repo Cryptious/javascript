b527b932432e35c64eaee9053263613e9c045445
Martino Wagey <martinowagey@gmail.com>
2018-08-14 13:51:30 +0700

    add objects Project

---
+++ objects.js
@@ -0,0 +1,11 @@
+function panggilObjects() {
+  var mobil = {
+    type: 'sedan',
+    harga: 200000000,
+    warna: "putih",
+  }
+  console.log(mobil);
+  console.log(mobil.harga);
+}
+
+panggilObjects()
