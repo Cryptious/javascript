a9bfaed9167db1c0b62ecb26a193e75b6302761e
Martino Wagey <martinowagey@gmail.com>
2018-08-14 16:13:56 +0700

    add array pop

---
+++ array-pop.js
@@ -0,0 +1,12 @@
+function panggilPop() {
+  var kota = ['jakarta','bandung','medan','makasar'];
+
+  console.log(kota);
+  console.log("===============");
+
+  var kota2 = kota.pop()
+  console.log(kota2);
+  return kota;
+}
+
+console.log(panggilPop());
