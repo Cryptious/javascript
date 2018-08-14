f83bb3770408544a1fc74966d726799da21031c4
Martino Wagey <martinowagey@gmail.com>
2018-08-14 13:44:11 +0700

    add project nested Array

---
+++ nested-array.js
@@ -0,0 +1,7 @@
+function panggilNestedArray() {
+  var dataNama = [["Ab", "Ac",["uhu3","uhuy4"]],
+                  ["Bola","bola2","bola3"],
+                  ["Rem1","Rem2","Rem3"]]
+  console.log(dataNama[0][2][1]);
+}
+panggilNestedArray()
