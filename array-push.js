833d8d4b6d7cf2c009c4912546b146037ef69ce2
Martino Wagey <martinowagey@gmail.com>
2018-08-14 16:19:42 +0700

    add array push

---
+++ array-push.js
@@ -0,0 +1,12 @@
+function panggilPush() {
+    var data = [1,2,3,4,5,6,7,8];
+
+
+    console.log(data);
+    data.push(9,'jakarta',10,11,12,13);
+
+    return data;
+
+}
+
+console.log(panggilPush());
