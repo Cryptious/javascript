9458c7d85101d58c1073bdca37faa4cc12eac58f
Martino Wagey <martinowagey@gmail.com>
2018-08-28 09:33:22 +0700

    add array concat

---
+++ array-concat.js
@@ -0,0 +1,13 @@
+function panggilConcat() {
+
+
+  var kota = ["Bandung","Jakarta","Bogor","Surabaya"]
+  var kabupaten = ["Jakarta Barat", "Bogor Selatan","Bandung timur","Surabaya Utara"]
+
+  var hasil = kota.concat(kabupaten)
+
+  return hasil;
+
+}
+
+console.log(panggilConcat());
