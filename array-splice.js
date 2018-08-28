84e1a4a977e1e139c46ab60b6713d74fbaeba110
Martino Wagey <martinowagey@gmail.com>
2018-08-28 08:56:38 +0700

    add array splice2

--- array-splice.js
+++ array-splice.js
@@ -1,10 +1,11 @@
 function panggilSplice() {
 
   var kota = ['jakarta','bandung','makasar','medan','manado','padang','malang','bogor'];
-
   console.log(kota);
 
-  kota.splice(2,0,'palembang')
+  // kota.splice(2,0,'palembang') add splice
+  kota.splice(4)
+
   return kota;
 
 }
