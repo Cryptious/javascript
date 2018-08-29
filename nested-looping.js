25f5b8d67a99093cf5d1b4db49994a869b44a9f1
Martino Wagey <martinowagey@gmail.com>
2018-08-29 12:05:13 +0700

    study kasus nested array dan looping

---
+++ nested-looping.js
@@ -0,0 +1,19 @@
+function panggilNestedArray(value){
+ // tulis jawabanmu disini
+
+ for (var i = 0; i < value.length; i++) {
+   console.log("id : " + value[i][0]);
+   console.log("name : " + value[i][1]);
+   console.log("company : " + value[i][2]);
+
+ }
+}
+
+var nestedArray = [
+  [1, "Mark Zuckerberg", "Facebook"],
+  [2, "Elon Musk", "Tesla"],
+  [3, "Bill Gates", "Microsoft"],
+  [4, "Steve Jobs", "Apple"]
+]
+
+panggilNestedArray(nestedArray)
