1e80634aa6d5f7be3b16ad2d41771e113fa14f0c
Martino Wagey <martinowagey@gmail.com>
2018-08-29 14:18:07 +0700

    add regexp with Flags 1

---
+++ flags-regexp.js
@@ -0,0 +1,12 @@
+function panggilRegexp(value) {
+
+
+  let str = "abcdefghijklmnopqrstuvwyz"
+
+  console.log(str.search(/k/));
+  console.log(str.search(/K/));
+  console.log(str.search(/K/i));
+
+}
+
+panggilRegexp()
