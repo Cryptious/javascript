d65d53c300f810a0788abb866367ca2f72386d7e
Martino Wagey <martinowagey@gmail.com>
2018-08-29 09:25:15 +0700

    add array reverse

---
+++ array-reverse.js
@@ -0,0 +1,11 @@
+function panggilReverse() {
+
+
+  var kota = ["Jakarta","Bandung","Bogor","Bali"]
+  console.log(kota);
+  console.log("===================");
+  var result = kota.reverse(kota)
+  return result;
+}
+
+console.log(panggilReverse());
