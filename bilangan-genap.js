fd78f04b20bb0869b67f00762b2af4e0acac80e1
Martino Wagey <martinowagey@gmail.com>
2018-08-29 09:37:43 +0700

    add bilangan genap

---
+++ bilangan-genap.js
@@ -0,0 +1,14 @@
+function panggilGenap() {
+
+
+  var bilanganGenap = []
+
+  for (var i = 1; i < 30; i++) {
+    if (i%2 === 0) {
+      bilanganGenap.push(i)
+    }
+  }
+  return bilanganGenap;
+}
+
+console.log(panggilGenap());
