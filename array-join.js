ec13eecac2fa4967d8079dd2c1329ec06a248954
Martino Wagey <martinowagey@gmail.com>
2018-08-29 09:21:25 +0700

    add array join

---
+++ array-join.js
@@ -0,0 +1,10 @@
+function panggilJoin() {
+
+
+  var kota = ["Jakarta","Bandung","Bogor","Tangerang"]
+  console.log(kota)
+  var result = kota.join(", ")
+  return result
+}
+
+console.log(panggilJoin());
