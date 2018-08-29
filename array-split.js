77cbdb3527187037c28f22ea895eb1befb9b6247
Martino Wagey <martinowagey@gmail.com>
2018-08-29 09:15:40 +0700

    add array split

---
+++ array-split.js
@@ -0,0 +1,10 @@
+function panggilSplit() {
+
+
+  var kalimat = "Kita Sedang Belajar js";
+  console.log(kalimat);
+  var result = kalimat.split(" ");
+  return result;
+}
+
+console.log(panggilSplit());
