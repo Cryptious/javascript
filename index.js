16299492672d4b40dedb3d58bc13f3bcedf550d0
Martino Wagey <martinowagey@gmail.com>
2018-10-05 09:44:10 +0700

    belajar js di browser

---
+++ index.js
@@ -0,0 +1,9 @@
+function panggilJavaScript() {
+   console.log("Menuliskan Code Javascript di Browser Secara external");
+}
+function panggilJs() {
+  console.log("function ke 2 extern");
+
+}
+ panggilJavaScript();
+ panggilJs();
